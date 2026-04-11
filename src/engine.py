0e9ea08972dd250b9c3ee77129454cdc9748725d
Dmitrii <bakaevdima4@gmail.com>
2026-04-11 16:07:05 +0300

    added simple csv data feed class

---
+++ src/engine.py
@@ -0,0 +1,30 @@
+import csv
+from DataClasses import Bar
+
+class DataFeedCSV():
+    def __init__(self, path: str):
+        self.path = path
+        self.file = None
+        self.reader = None
+
+    def reset(self):
+        if self.file:
+            self.file.close()
+
+        self.file = open(self.path, "r")
+        self.reader = csv.DictReader(self.file)
+
+    def __iter__(self):
+        self.reset()
+        return self
+
+    def __next__(self):
+        row = next(self.reader)
+        return Bar(
+            timestamp=int(row["timestamp"]),
+            open=float(row["open"]),
+            high=float(row["high"]),
+            low=float(row["low"]),
+            close=float(row["close"]),
+            volume=float(row["volume"]),
+        )
